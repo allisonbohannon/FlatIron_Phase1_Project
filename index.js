20b458678bd2fedc4772578356c8ce950a383699
Allison Bohannon <akboh.github@gmail.com>
2022-06-03 20:36:41 -0700

    Add book photos to search

--- index.js
+++ index.js
@@ -61,16 +61,18 @@ function renderSearchResults(results) {
 //Each card has two buttons: one to add to a want to read list, the other to an already read list
 //The card id is the book's isbn, which will be used as a key to add a book to the other lists
 function renderSearchResult(result) {
-    const url = `https://covers.openlibrary.org/b/ISBN/${result.isbn[0]}-S.jpg`
+    const url = `https://covers.openlibrary.org/b/ISBN/${result.isbn[0]}-M.jpg`
     const resultCard = document.createElement('li'); 
     resultCard.className = 'result-card'
     resultCard.id = result.isbn[0]
     resultCard.innerHTML = `
-        <img src = ${url}>
+        <section>
         <h4><strong>Title: <em>${result.title}</em></strong></h3>
         <p><strong>Author(s)</strong>: ${result.author_name}</h3>
         <p class = 'published-year' >Published: ${result.first_publish_year}</p>
         <button class = 'add-read-button'>add read</button>
+        </section>
+        <section class ='img-container'><img src = ${url}></section>
     `
     resultCard.querySelector('.add-read-button').addEventListener('click', (e) => {
         const isbn = e.target.parentNode.id; 
